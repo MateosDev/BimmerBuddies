d100f46c067136bdbcacf2b840170cd00bce69df
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-03 01:22:32 -0600

    server file done

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api*json"}));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/api-routes")(app);
+require("./app/routing/html-routes")(app);
+
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
