991161531a2ccd745c83b987cad727f425f51b52
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-03 01:51:18 -0600

    working on public folder after getting everything in the friends.js file. using questions from the last attempt, they were fine, that wasn't the issue

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendsData = [
+    {
+      Username: "Allie Smith",
+      photo:"https://i.pinimg.com/originals/70/81/70/708170de0051108c5d508b69f1dce819.jpg",
+      scores: ["5", "3", "3", "1", "2", "4", "5", "2", "2", "5"]
+    },
+    {
+      Username: "Billie Ackley",
+      photo: "https://i.pinimg.com/originals/61/4f/10/614f107a849571098f95dd255f01fc16.jpg",
+      scores: ["2", "1", "1", "4", "2", "4", "4", "2", "4", "2"]
+    },
+    {
+      Username: "Gina Kroft",
+      photo:"https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/27574071_585798905088246_36465075501924352_n.jpg?_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=106&_nc_ohc=saJJeEpFt4gAX94jXz2&oh=39036207650a52ce116091e66b516da1&oe=5EAC6222",
+      scores: ["1", "2", "2", "5", "2", "4", "5", "2", "3", "3"]
+    },
+    {
+        Username: "Jessie Lumden",
+        photo:"https://i.pinimg.com/originals/96/cc/e4/96cce4e5a3a4ea9e9b197603b52ce31f.jpg",
+        scores: ["4", "3", "1", "4", "2", "4", "3", "3", "1", "2"]
+      },
+      {
+        Username: "Starr Shay",
+        photo:"https://scontent-dfw5-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/22069104_377215399363762_3740814307479257088_n.jpg?_nc_ht=scontent-dfw5-1.cdninstagram.com&_nc_cat=110&_nc_ohc=7vsRbzq7plQAX-F93t8&oh=d94f5e43f6aec43c8c3b52e864131f1f&oe=5E96AA8F",
+        scores: ["3", "4", "5", "1", "2", "4", "3", "2", "3", "5"]
+      },
+      {
+        Username: "Leia Pearson",
+        photo:"https://scontent-dfw5-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p750x750/27878523_567027006978763_3721213601453703168_n.jpg?_nc_ht=scontent-dfw5-2.cdninstagram.com&_nc_cat=107&_nc_ohc=oHreWJJOkX0AX8Aj_gE&oh=bc4930a6d10103262b73e81a93e453d1&oe=5EA6B031",
+        scores: ["5", "5", "5", "4", "5", "4", "3", "3", "3", "4"]
+      },
+  ];
+  
+  module.exports = friendsData;
